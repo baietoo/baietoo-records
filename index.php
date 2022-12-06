eb3bd83d33527d1e7a9aa9652512c21d116c5f27
Catalin Circu <catalingcircu@gmail.com>
2022-12-06 08:12:58 +0200

    redid connection to mysql

--- index.php
+++ index.php
@@ -18,22 +18,20 @@
 
     <?php
 
-        $cleardb_url = parse_url(getenv("CLEAR_DB_DATABASE_URL"));
-        $cleardb_server = $cleardb_url["host"];
-        $cleardb_user = $cleardb_url["user"];
-        $cleardb_pass = $cleardb_url["pass"];
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-        $cleardb_db = substr($cleardb_url["path"], 1);
+        $server = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $db = substr($url["path"], 1);
 
-        $active_group = 'default';
-        $query_builder = TRUE;
+        $conn = new mysqli($server, $username, $password, $db);
 
-        $conn = new mysqli($cleardb_server, $cleardb_user, $cleardb_pass, $cleardb_db);
-        if($conn->connect_error) {
-            die("Connection Failed" . $conn->connect_error);
-        }
+        // if($conn->connect_error) {
+        //     die("Connection Failed" . $conn->connect_error);
+        // }
 
-        $sql = "SELECT prenume from test_table";
+        $sql = "SELECT * from test_table";
         $result = $conn->query($sql);
 
         if($result->num_rows > 0){
