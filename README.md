a2b1d02d7180e12fe17afafe3b9ef2af6abb1bed
Catalin Circu <catalingcircu@gmail.com>
2022-12-10 21:34:58 +0200

    [BOOTSRAP] integrated into project via composer

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # baietoo-records
+
+## CERINTE
+- Proiectul va utiliza o baza de date MySQL si va fi programat în PHP. 
+    - conexiune definita, de separat in alt modul
+- Prin intermediul aplicatiei, se vor efectua operatii de stergere, adaugare, citire asupra bazei de date.
+    - TODO
+- Va exista o pagina de autentificare/înregistrare de utilizatori.
+- Vor exista mai multe categorii de utilizatori. Fiecare categorie va avea anumite actiuni specifice.
+- Aplicatia va contine mai multe pagini dinamice cu legaturi între ele. 
+- Va exista posibilitatea de generare si vizualizare de rapoarte (nu doar HTML si PHP).
+- Elemente statistice ale site-ului: vizitatori, accesari etc.
+- Formular de contact, cu posibilitatea trimiterii de email-uri.
+- Integrarea de informatii (nu pagini în întregime, ci doar elemente ale acestora) de pe alte site-uri.
+- Terminarea sesiunii.
--- index.php
+++ index.php
@@ -1,9 +1,61 @@
 <html>
 <head>
-    <title>ow LORD, php</title>
+    <title>Baietoo Records</title>
+    <link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"> </script>
 </head>
+
 <body>
+
+<header>
+
+    <?php $nume_site = "BaietooRecords"?> 
+
+    <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <?php echo $nume_site ?>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Acasa</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Trimite-ne melodia ta!</a>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Zona Artistului
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Melodii</a></li>
+                <li><a class="dropdown-item" href="#">Date Personale</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Plata</a></li>
+            </ul>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link disabled">Contul Meu</a>
+            </li>
+        </ul>
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        </div>
+    </div>
+    </nav>
+</header>
+
+<main class="m-4">
+    
     <?php
+        // learning some basic concepts
+        /*
         define('AN_FONDARE', 2010);
         $nume = "BaietooRecords";
         echo "$nume - Casa de productie muzicala<br />";
@@ -14,6 +66,7 @@
         $$varname = 600.5;
         echo "$nume - a mai facut: $bani de bani!<br />";
         echo AN_FONDARE;
+        */
     ?>
 
     <?php
@@ -36,12 +89,29 @@
 
         if($result->num_rows > 0){
             while($row = $result->fetch_assoc()) {
-                echo "nume: " . $row['nume'] . " - Prenume:" . $row['prenume'] . "<br />";
+                $nume = $row['nume'];
+                $prenume = $row['prenume'];
+                // echo "nume: " . $row['nume'] . " - Prenume:" . $row['prenume'] . "<br />";
             }
         } else {
             echo "0 results";
         }
         $conn->close();
     ?>
+
+    <div class="card">
+        <div class="card-body">
+            <?php echo $nume . " " . $prenume ?>
+        </div>
+    </div>
+    
+
+</main>
+
+<footer>
+
+</footer>
+
+
 </body>
 </html>
