7d6013a4a79b5386db7b1c1f50e8c264bee99925
Catalin Circu <baietoo@192-168-0-152.rdsnet.ro>
2023-05-01 19:44:38 +0300

    added localhost variables adn heroku in config.php

--- app/config/config.php
+++ app/config/config.php
@@ -3,15 +3,20 @@
     define('APP_ROOT', dirname(dirname(__FILE__)) );
     // URL Root 
     // TODO: fix for heroku
-    define('URL_ROOT', 'http://localhost/baietoo-records');
+    // if running on localhost
+    if($_SERVER['SERVER_NAME'] == 'localhost'){
+        define('URL_ROOT', 'http://localhost/baietoo-records');
+        $url = 'mysql://b78a0ed438279c:aea489eb@eu-cdbr-west-03.cleardb.net/heroku_466887521e2d644?reconnect=true';
+    } else {
+        define('URL_ROOT', '/');
+        // DATABASE URL
+        define('URL', parse_url(getenv("CLEARDB_DATABASE_URL")));
+    }
     // Site Name
     define('SITE_NAME', 'BaietooRecords');
-
+    
     // DATABASE PARAMETERS
-    define('URL', parse_url(getenv("CLEARDB_DATABASE_URL")));
-    // TODO: include CLEARDB variable in debug
-    // $url = 'mysql://b78a0ed438279c:aea489eb@eu-cdbr-west-03.cleardb.net/heroku_466887521e2d644?reconnect=true';
-    // define('URL', parse_url($url));
+    define('URL', parse_url($url));
     define('DB_HOST',URL["host"]);
     define('DB_USER',URL["user"]);
     define('DB_PASS',URL["pass"]);
