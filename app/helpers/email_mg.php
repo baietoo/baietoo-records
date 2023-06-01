b75c1d99bc9cba592c0fb6e6d4061bfc3c1ef728
Catalin Circu <baietoo@192-168-0-152.rdsnet.ro>
2023-06-01 07:10:09 +0300

    admin view: preview all contacts/issues via contact form

--- app/helpers/email_mg.php
+++ app/helpers/email_mg.php
@@ -10,10 +10,6 @@ define('PUBLIC_API_KEY','key-5bda46853741ca8990d361ddf134a939');
 define('API_BASE_URL', 'https://api.mailgun.net/v3/sandbox91d3b430611142f793f3e07d77c21a69.mailgun.org');
 define('DOMAIN', "sandbox91d3b430611142f793f3e07d77c21a69.mailgun.org");
 
-# Instantiate the client.
-// https://github.com/mailgun/mailgun-php/issues/646
-
-
 function sendEmail($data){
     $email = $data['email'];
     $body = $data['body'];
@@ -25,5 +21,62 @@ function sendEmail($data){
         'text'    => $body
     ]);
     return true;
+}
+
+function readEmails(){
+    $emails = array();
+    $mg = Mailgun::create(PRIVATE_API_KEY);
+    #returns a list of 25 messages
+    $queryString = array(
+        // 'begin'        => 'Wed, 1 May 2023 09:00:00 -0000',
+        // 'ascending'    => 'yes',
+        // 'limit'        =>  25,
+        // 'pretty'       => 'yes',
+        'recipient'    => 'vgnzd@inbox.testmail.app',
+        'event'        => 'delivered'
+        // 'event'       => 'accepted OR delivered OR failed OR opened OR clicked OR unsubscribed OR complained OR stored' 
+    );
+    $result = $mg->events()->get(DOMAIN, $queryString);
+    $items = $result->getItems();
+    for($i = 0; $i < count($items); $i++){
+        $storage = $items[$i]->getStorage();        
+        $emails[$i]['from'] = $items[$i]->getMessage()['headers']['from'];
+        $emails[$i]['subject'] = $items[$i]->getMessage()['headers']['subject'];
+        $emails[$i]['body'] = getMessageBody($storage);
+    }
+
+    // $item = $emails->getItems()[0];
+    // $idMesaj = $item->getMessage()['headers']['message-id'];
+    // $deLa = $item->getMessage()['headers']['from'];
+    // $subiect = $item->getMessage()['headers']['subject'];
+    // var_dump($item->getStorage());
+    // $ch = curl_init();
+    // curl_setopt($ch, CURLOPT_URL, "https://api.mailgun.net/v3/domains/sandbox91d3b430611142f793f3e07d77c21a69.mailgun.org/messages/$idMesaj");
+    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+    // $headers = array();
+    // return $result->http_response_body->items;
+    return $emails;
+}
+
+function getMessageBody($storage){
+    // $mg = Mailgun::create(PRIVATE_API_KEY);
+    // $queryString = array(
+    //     'message-id'        => 'delivered'
+    // );
+    // $result = $mg->events()->get(DOMAIN, $queryString);
+    // $result = $mg->messages()->get(DOMAIN, $message_id);
+    // return $result->http_response_body->body;
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $storage['url']);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERPWD, "api:".PRIVATE_API_KEY);
+    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    $output = curl_exec($ch);
+    $jo = json_decode($output,true);
+    curl_close($ch);
+    return $jo['body-plain'];
+    // return $storage->get('body-html');
 }
 ?>
--- app/views/admins/index.php
+++ app/views/admins/index.php
@@ -2,6 +2,16 @@
 <h1>Welcome Admin</h1>
 <h2>Tasks for today: </h2>
 <!-- load emails from mailgun -->
+<?php $emails = readEmails(); ?>
+<?php foreach($emails as $email): ?>
+    <div class="card card-body mb-3">
+        <h4 class="card-title"><?php echo $email['subject']; ?></h4>
+        <div class="bg-light p-2 mb-3">
+            <?php echo $email['body']; ?>
+        </div>
+        <p class="card-text">From: <?php echo $email['from']; ?></p>
+    </div>
+<?php endforeach; ?>
 
 
 <?php require APP_ROOT . '/views/inc/footer.php'; ?>
